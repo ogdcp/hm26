d0c9e5e998b1ab4db94e0e8f99b785ee4aaad752
робот <vaginalnaymamba@mail.ru>
2023-06-15 23:19:26 +0300

    start hm

---
+++ src/main/java/com/example/hm26/Hm26Application.java
@@ -0,0 +1,13 @@
+package com.example.hm26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hm26Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hm26Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hm26/Hm26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hm26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hm26ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
